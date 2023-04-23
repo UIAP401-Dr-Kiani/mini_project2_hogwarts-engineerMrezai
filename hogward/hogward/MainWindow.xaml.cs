6be57e08f09d153d0ad7c4528b0f57a9f01afd32
Mahdi Rezai <engineer.m.rezai@gmail.com>
2023-04-23 12:43:07 +0430

    Main windwo get smarter: now you enter the user name and password and choise your type in same page

--- hogward/hogward/MainWindow.xaml.cs
+++ hogward/hogward/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using hogward.Windows;
+using System.IO;
+using System.Windows;
 namespace hogward
 {
     public partial class MainWindow : Window
@@ -9,20 +11,53 @@ namespace hogward
 
         }
 
-        private void Dumbledore_OnClick(object sender, RoutedEventArgs e)
-        {
-            DumbledoreLogin dumbledore = new DumbledoreLogin();
-            dumbledore.Show();
-        }
-
-        private void Teachers_OnClick(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void Students_OnClick(object sender, RoutedEventArgs e)
         {
-
+            try 
+            {
+                if (Type.Text == "Dumbledore")
+                {
+                    if (UserName.Text == "Admin" && PassWord.Text == "Admin")
+                    {
+                        DumbledoreWorkDesk dumbledoreWorkDesk = new DumbledoreWorkDesk();
+                        dumbledoreWorkDesk.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        throw new System.Exception();
+                    }
+                }
+                else if (Type.Text == "Professor")
+                {
+                    if (UserName.Text == "" && PassWord.Text == "")
+                    { }
+                    else
+                    { 
+                        throw new System.Exception(); 
+                    }
+                }
+                else if (Type.Text == "Student")
+                {
+                    if (UserName.Text == "" && PassWord.Text == "")
+                    { }
+                    else
+                    {
+                        throw new System.Exception(); 
+                    }
+                }
+            }
+            catch
+            {
+                using (StreamWriter ln = new StreamWriter("Error.txt"))
+                {
+                    ln.WriteLine("UserName or Password not Found");
+                    ln.Close();
+                }
+                Error_page error_Page = new Error_page();
+                error_Page.Show();
+            }
         }
     }
 }
+
