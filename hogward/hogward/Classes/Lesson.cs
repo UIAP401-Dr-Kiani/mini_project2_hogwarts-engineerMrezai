9b19560f35e66ff0b63b48c035bfe2c7d026af97
Mahdi Rezai <engineer.m.rezai@gmail.com>
2023-04-23 11:50:30 +0430

    Classes added to read they data from file in lessen.cs

--- hogward/hogward/Classes/Lesson.cs
+++ hogward/hogward/Classes/Lesson.cs
@@ -1,10 +1,169 @@
-﻿namespace hogward;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace hogward;
 
 public class Lesson
 {
-    protected string Time;
+    protected int Time = 2;
     protected int NumberOfStudents;
-    protected int Capacity;
-    protected string PresentationSemester;
+    protected int Capacity = 50;
+    protected int PresentationSemester;
     protected string Name;
+    protected Lesson()
+    {
+        try { }
+        catch (System.NullReferenceException e)
+        {
+            using (StreamWriter ln = new StreamWriter("Error.txt"))
+            {
+                ln.WriteLine(e.Message);
+                ln.Close();
+            }
+        }
+    }
+}
+
+
+class Chemistry : Lesson
+{
+    Chemistry()
+    {
+        Name = "Chemistry";
+        PresentationSemester = 1;
+        try
+        {
+            string[] Chemicals = (File.ReadAllText("Chemistry/Chemicals.txt").Split("-"));
+        }
+        catch
+        {
+            throw new System.NullReferenceException(string.Format("No Chemicals.txt"));
+        }
+    }
+}
+
+
+class BotanicalOne : Lesson
+{
+    BotanicalOne()
+    {
+        PresentationSemester = 1;
+        Name = "BotanicalOne";
+        
+        try
+        {
+            string[] Chemicals = File.ReadAllText("Botanical/Planet.txt").Split("-");
+        }
+        catch
+        {
+            throw new System.NullReferenceException(string.Format("No Plant for BotanicalOne"));
+        }
+    }
+
+}
+
+class BotanicalTwo : Lesson
+{
+    BotanicalTwo()
+    {
+        PresentationSemester = 2;
+        Name = "BotanicalTwo";
+        try
+        {
+            string[] Chemicals = File.ReadAllText("Botanical/Planet.txt").Split("-");
+        }
+        catch
+        {
+            throw new System.NullReferenceException(string.Format("No No Plant for BotanicalTwo"));
+        }
+    }
+}
+
+class BotanicalThree : Lesson
+{
+    BotanicalThree()
+    {
+        PresentationSemester = 3;
+        Name = "BotanicalThree";
+        try
+        {
+            string[] Chemicals = File.ReadAllText("Botanical/Planet.txt").Split("-");
+        }
+        catch
+        {
+            throw new System.NullReferenceException(string.Format("No Plant for BotanicalThree"));
+        }
+    }
+}
+
+class BotanicalFour : Lesson
+{
+    BotanicalFour()
+    {
+        PresentationSemester = 4;
+        Name = "BotanicalFour";
+        try
+        {
+            string[] Chemicals = File.ReadAllText("Botanical/Planet.txt").Split("-");
+        }
+        catch
+        {
+            throw new System.NullReferenceException(string.Format("No Plant for BotanicalFour"));
+        }
+    }
+}
+
+class Occultism : Lesson
+{
+    Occultism()
+    {
+        PresentationSemester = 2;
+        Name = "Occultism";
+        try
+        {
+            string[] Words = File.ReadAllText("Occultism/Words.txt").Split('-');
+        }
+        catch
+        {
+            throw new System.NullReferenceException(string.Format("No Word"));
+        }
+    }
+}
+
+
+class Sport : Lesson
+{
+    Sport()
+    {
+        PresentationSemester = 3;
+        try
+        {
+            Name = "Sport";
+            string[] TypeOfSport = File.ReadAllText("Sport/TypeOfSport.txt").Split('-');
+        }
+        catch
+        {
+            throw new System.NullReferenceException(string.Format("No sport"));
+        }
+    }
+}
+
+
+class Plant : Lesson
+{
+
+    Plant()
+    {
+        PresentationSemester = 4;
+        try
+        {
+            string[] Chemicals = File.ReadAllText("Botanical/Planet.txt").Split("-");
+        }
+        catch
+        {
+            throw new System.NullReferenceException(string.Format("No Plant"));
+        }
+    }
 }
--- hogward/hogward/MainCode.cs
+++ hogward/hogward/MainCode.cs
@@ -1,38 +1,12 @@
 ﻿using hogward;
 using System.Collections.Generic;
 
-class Chemistry : Lesson
-{
-    List<string> chemicals = new List<string>();
-}
 
 
-class Botanical : Lesson
-{
-    List<string> Semester1Plants;
-    List<string> Semester2Plants;
-    List<string> Semester3Plants;
-    List<string> Semester4Plants;
-}
 
 
-class Occultism : Lesson
-{
-    List<string> Words;
-}
 
 
-class Sport : Lesson
-{
-    string TypeOfSport;
-}
-
-
-class Plant : Lesson
-{
-    string Name;
-    int Count;
-}
 
 class Drom
 {
